5035430a4e081045bea367249b812665a0512f25
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2016-11-06 21:49:55 +0100

    Initial Commit

---
+++ src/main/java/com/github/alexandrenavarro/shade/Toto.java
@@ -0,0 +1,20 @@
+package com.github.alexandrenavarro.shade;
+
+import org.springframework.cloud.sleuth.Span;
+
+/**
+ * Created by anavarro on 05/11/16.
+ */
+public class Toto {
+
+    private Span span;
+
+    // org.springframework.cloud.sleuth.Span
+    //import org.springframework.cloud.sleuth.Log;
+//import org.springframework.util.Assert;
+//import org.springframework.util.CollectionUtils;
+//import org.springframework.util.MultiValueMap;
+//import org.springframework.util.ObjectUtils;
+//import org.springframework.util.StringUtils;
+
+}
